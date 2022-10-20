54a864a0700342f1c89767150f30e45c4f4fb844
Arnab Mitra <mitraarnab.2013@gmail.com>
2022-10-20 09:38:42 +0530

    Adding Files

---
+++ Front End Development Libraries/React/Program-2.jsx
@@ -0,0 +1,10 @@
+const JSX = 
+<div>
+<h1>Hey there!!!</h1>
+<p>Hello, welcome to my first App</p>
+<ul>
+<li>First</li>
+<li>Second</li>
+<li>Third</li>
+</ul>
+</div>;
---
+++ Front End Development Libraries/React/Program-4.jsx
@@ -0,0 +1,11 @@
+const JSX = (
+    <div>
+      <h1>This is a block of JSX</h1>
+      <p>Here's a subtitle</p>
+      {/*<ul>
+      <li>First</li>
+      <li>Second</li>
+      <li>Third</li>
+      </ul> */}
+    </div>
+  );
